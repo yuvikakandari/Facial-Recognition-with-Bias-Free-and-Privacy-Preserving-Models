55d336f6a97878debcd983e5c04dc8a2b7e29b4f
Yuvika Kandari <yuvika7kandari@gmail.com>
2026-03-23 12:01:04 +0530

    encrypting face data

---
+++ encryption.py
@@ -0,0 +1,9 @@
+from cryptography.fernet import Fernet
+
+def generate_key():
+    key = Fernet.generate_key()
+    with open("secret.key", "wb") as key_file:
+        key_file.write(key)
+
+def load_key():
+    return open("secret.key", "rb").read()
